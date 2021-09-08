8471f4497324c3103eeb23796cc9e215aababf98
Vadim-Cherepanov-Web-Engineer <vadim.rgsu@yandex.ru>
2021-09-09 01:34:32 +0300

    ПР 12 завершена

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import Main from "./Main";
+import {Route, Switch, useHistory} from 'react-router-dom';
 import Footer from "./Footer";
 import PopupWithForm from "./PopupWithForm";
 import ImagePopup from "./ImagePopup";
@@ -9,14 +10,26 @@ import CurrentUserContext from "../contexts/CurrentUserContext";
 import AddPlacePopup from "../components/AddPlacePopup";
 import EditProfilePopup from "../components/EditProfilePopup";
 import EditAvatarPopup from "../components/EditAvatarPopup";
+import Login from "./Login";
+import Register from "./Register";
+import ProtectedRoute from "./ProtectedRoute";
+import InfoTooltip from "./InfoTooltip";
+import * as auth from "../utils/auth"
+import done from "../images/done.svg";
+import fail from "../images/fail.svg";
 
 function App() {
-  const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = React.useState(false);
+  const [loggedIn, setLoggedIn] = React.useState(false);
+	const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = React.useState(false);
   const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = React.useState(false);
   const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = React.useState(false);
   const [selectedCard, setSelectedCard] = React.useState(null);
   const [currentUser, setCurrentUser] = React.useState({});
   const [cards, setCards] = React.useState([]);
+	const [userEmail, setUserEmail] = React.useState('');
+  const [isInfoTooltipOpen, setInfoTooltipOpen] = React.useState({ opened: false, success: false });
+  const [currentPath, setCurrentPath] = React.useState('/')
+  const history = useHistory();
 
   React.useEffect(() => {
     api
@@ -92,6 +105,7 @@ function App() {
     setIsEditProfilePopupOpen(false);
     setIsAddPlacePopupOpen(false);
     setSelectedCard(null);
+		setInfoTooltipOpen({ opened: false, success: false})
   };
 
   React.useEffect(() => {
@@ -142,19 +156,108 @@ function App() {
 		});
 	}
 
+	const handlePathChange = (newPath) => {
+		setCurrentPath(newPath);
+	 }
+
+	// Проверка токена:
+	React.useEffect(() => {
+    auth.tokenCheck(localStorage.getItem('token'))
+    .then(result => {
+      if (result) {
+        setUserEmail(result.data.email);
+        setLoggedIn(true);
+        history.push('/');
+        setCurrentPath('/');
+      } else {
+        throw new Error ('Ошибка текущего сеанса. Необходимо заново авторизироваться')
+      }
+    })
+    .catch (err => {
+      console.log(`Ошибка входа по токену ${err}`);
+      history.push('/sign-in');
+    })
+  }, [])
+
+	 // Обработчик завершения:
+	 const handleLogout = () => {
+		localStorage.removeItem('token');
+		setUserEmail('');
+		setLoggedIn(false);
+		history.push('/sign-in');
+		setCurrentPath('/sign-in');
+	}
+
+	 // Обработчик регистрации:
+	 const handleSignupSubmit = (email, password) => {
+		 auth.register (email, password)
+		 .then(result => {
+			 if (result) {
+				 setUserEmail(result.data.email);
+				 setInfoTooltipOpen({ opened: true, success: true })
+				 setLoggedIn(true);
+				 history.push('/sign-in');
+				 setCurrentPath('/sign-in');
+			 }
+			 else {
+				 throw new Error('Не удалось пройти регистрацию');
+			 }
+		 })
+		 .catch( err => {
+		 console.log(`Ошибка регистрацииЖ ${err}`);
+		 setInfoTooltipOpen({ opened: true, success: false })
+	 })
+ }
+
+	 // Обработчик авторизации:
+	 const handleSigninSubmit = (email, password) => {
+		 auth.authorization (email, password)
+		 .then(data => {
+			 if (data.token) {
+				 localStorage.setItem('token', data.token);
+				 setUserEmail(email);
+				 setLoggedIn(true);
+				 history.push('/');
+				 setCurrentPath('/');
+			 }
+			 else {
+				 throw new Error('Не удалось получить токен от сервера');
+			 }
+		 })
+		 .catch( err => {
+			 console.log(alert(`Ошибка авторизации ${err}. Проверьте корректность данных`))
+	 })
+	 }
+
   return (
     <CurrentUserContext.Provider value={currentUser}>
-      <Header></Header>
-      <Main
-        onEditAvatar={handleEditAvatarClick}
-        onEditProfile={handleEditProfileClick}
-        onAddPlace={handleAddPlaceClick}
-        onCardClick={handleCardClick}
-        onCardLike={handleCardLike}
-        onCardDelete={handleCardDelete}
-        cards={cards}
-        onSubmit={handleAddPlaceSubmit}
-      ></Main>
+      <Header
+			userEmail={userEmail}
+			onLogout={handleLogout}
+			path={currentPath} />
+
+			<Switch>
+        <Route path='/sign-in'>
+          <Login onSignin={handleSigninSubmit} onPathChange={handlePathChange}/>
+        </Route>
+        <Route path='/sign-up'>
+          <Register onSignup={handleSignupSubmit} onPathChange={handlePathChange} />
+        </Route>
+
+
+				<ProtectedRoute path='/'
+      		loggedIn={loggedIn}
+					component={Main}
+        	onEditAvatar={handleEditAvatarClick}
+        	onEditProfile={handleEditProfileClick}
+        	onAddPlace={handleAddPlaceClick}
+        	onCardClick={handleCardClick}
+        	onCardLike={handleCardLike}
+        	onCardDelete={handleCardDelete}
+        	cards={cards}
+        	onSubmit={handleAddPlaceSubmit}
+				/>
+			</Switch>
       <Footer></Footer>
 
       <EditProfilePopup
@@ -169,6 +272,12 @@ function App() {
           closeAllPopups();
         }}
       ></ImagePopup>
+
+			<InfoTooltip
+        isOpen={isInfoTooltipOpen.opened}
+        onClose={closeAllPopups}
+        statusImage={isInfoTooltipOpen.success ? done : fail}
+        title={isInfoTooltipOpen.success ? 'Вы успешно зарегистрировались!':'Что-то пошло не так! Попробуйте ещё раз'} />
 
       <PopupWithForm
         title="Вы уверены?"
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,34 @@
 import React from "react";
-import logo from "../images/logo.svg"
+import logo from "../images/logo.svg";
+import { Link } from 'react-router-dom';
 
-function Header() {
-  return (
+function Header(props) {
+  const newPath = props.path === '/' || props.path === '/sign-up' ? '/sign-in' : '/sign-up';
+  const linkName = {'/': 'Выйти', '/sign-up': 'Войти', '/sign-in': 'Регистрация'}
+  const handleLogout = () => {props.onLogout()};
+  console.log(props.path);
+
+	return (
     <header className="header">
       <img className="header__logo" src={logo} alt="Логотип сайта Mesto" />
+      {props.path === "/" ? (
+        <div className="header__menu">
+          <a className="header__link header__link_type_email">
+            {props.userEmail}
+          </a>
+          <Link
+            className="header__link header__link_type_exit"
+            to={newPath}
+            onClick={handleLogout}
+          >
+            {linkName[props.path]}
+          </Link>
+        </div>
+      ) : (
+        <Link className="header__link" to={newPath}>
+          {linkName[props.path]}
+        </Link>
+      )}
     </header>
   );
 }
